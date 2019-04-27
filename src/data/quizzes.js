e08d6dd1adf40719f137afaa51da5c9ad5a60239
KenavR <kenavr@gmail.com>
2019-04-27 23:31:46 +0200

    Changed data imported into FB

--- src/data/quizzes.js
+++ src/data/quizzes.js
@@ -3,7 +3,357 @@ export default [
     title: 'Who wants to be a millionaire?',
     reward: 300,
     category: 'Gameshows',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Comics',
     imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Comics',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: '00s',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: 'Music',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: 'Gameshows',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Comics',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: '00s',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: 'Music',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: 'Gameshows',
+    imagePath: '/assets/img/mountain.jpg',
     description: '',
     timePerQuestion: 30,
     questions: [
@@ -46,6 +396,131 @@ export default [
         text: 'Which malformation did Marilyn Monroe have when she was born?',
         answer: 'Six toes',
         options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'TV Early 00s',
+    reward: 300,
+    category: 'Film/TV',
+    imagePath: '',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'How many oscars did the Titanic movie got?',
+        answer: '11',
+        options: ['5', '7', '9']
+      },
+      {
+        text: 'How many Tomb Raider movies were made?',
+        answer: '2',
+        options: ['3', '1', '4']
+      },
+      {
+        text: 'Which malformation did Marilyn Monroe have when she was born?',
+        answer: 'Six toes',
+        options: ['Crooked Nose', 'A Penis', 'No hair']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: '00s',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
+      }
+    ]
+  },
+  {
+    title: 'Who wants to be a millionaire?',
+    reward: 300,
+    category: 'Music',
+    imagePath: '/assets/img/mountain.jpg',
+    description: '',
+    timePerQuestion: 30,
+    questions: [
+      {
+        text: 'Which disease devastated livestock across the UK during 2001?',
+        answer: 'Foot-and-mouth',
+        options: ['Hand-and-foot', 'Foot-in-mouth', 'Hand-to-mouth']
+      },
+      {
+        text: 'Which of these kills its victims by constriction?',
+        answer: 'Anaconda',
+        options: ['Andalucia', 'Andypandy', 'Annerobinson']
+      },
+      {
+        text: 'Which of these might be used in underwater naval operations?',
+        answer: 'Frogmen',
+        options: ['Newtmen', 'Toadmen', 'Tadpolemen']
       }
     ]
   }
